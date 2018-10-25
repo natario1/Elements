c904e7f931288e0e19e1b771580317397fd6a433
Mattia Iavarone <mat.iavarone@gmail.com>
2018-10-25 16:02:31 +0200

    Add more logs, fix SourceResultsPager

--- library/src/main/kotlin/com/otaliastudios/elements/pagers/SourceResultsPager.kt
+++ library/src/main/kotlin/com/otaliastudios/elements/pagers/SourceResultsPager.kt
@@ -2,18 +2,27 @@ package com.otaliastudios.elements.pagers
 
 import com.otaliastudios.elements.*
 import com.otaliastudios.elements.extensions.MainSource
+import kotlin.math.abs
 
 open class SourceResultsPager(
-        val fraction: Float,
-        val selector: (Source<*>) -> Boolean = { it is MainSource }): Pager() {
+        val fraction: Float = 0.8F,
+        val selector: (source: Source<*>, elementType: Int) -> Boolean = { source, elementType ->
+            source is MainSource
+                    && elementType != MainSource.ELEMENT_TYPE_LOADING
+                    && elementType != MainSource.ELEMENT_TYPE_EMPTY
+                    && elementType != MainSource.ELEMENT_TYPE_ERROR
+        }): Pager() {
 
     override fun onElementBound(page: Page, element: Element<*>, presenter: Presenter<*>, absolutePosition: Int, pagePosition: Int) {
         val source = element.source
-        if (page.isLastPage() && selector(source) && source.hasResultsForPage(page)) {
+        if (page.isLastPage() && selector(source, element.type) && source.hasResultsForPage(page)) {
             val sourceResults = source.getResultsForPage(page)
             val sourcePosition = sourceResults.indexOfFirst { it === element }
+            ElementsLogger.v("SourceResultsPager: source matches and has ${sourceResults.size} results. " +
+                    "The position of this element is $sourcePosition. (in page: $pagePosition, absolute: $absolutePosition, data: ${element.data})")
             if (sourcePosition >= 0) {
                 val trigger = (sourceResults.size * fraction).toInt()
+                ElementsLogger.w("SourceResultsPager: comparing with ${trigger - 1}")
                 if (sourcePosition == maxOf(trigger - 1, 0)) {
                     openPage()
                 }
